b22da98383e0becec8fb57d061f50ef9aadd43d2
cjh <flyflow4@gmail.com>
2026-05-04 17:24:06 +0900

    -- 20260504 객체지향원리

--- src/main/java/com/project/core/AppConfig.java
+++ src/main/java/com/project/core/AppConfig.java
@@ -2,33 +2,42 @@ package com.project.core;
 
 import com.project.core.discount.DiscountPolicy;
 import com.project.core.discount.FixDiscountPolicy;
+import com.project.core.discount.RateDiscountPolicy;
 import com.project.core.member.MemberRepository;
 import com.project.core.member.MemberService;
 import com.project.core.member.MemberServiceImpl;
 import com.project.core.member.MemoryMemberRepository;
 import com.project.core.order.OrderService;
 import com.project.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 구현
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
     // 역할
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 구현
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 역할
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
